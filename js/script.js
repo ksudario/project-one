302b894231a66f1d734da56ef276352e42642bc7
ksudario <60546566+ksudario@users.noreply.github.com>
2020-05-13 18:48:53 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,13 +5,11 @@ $(document).ready(function(){
         $.ajax({
           url: 'https://api.spoonacular.com/recipes/search?query='+search+'&number=5&apiKey=f95e57ef771341bda59fb9162db6da94'
       }).then(function(data){
-          // alert(data.results[0].title);
           for (var i = 0; i <5; i++) {
-          var   dat ='<dl><dt>Recipe:'+data.results[i].title+'</dt><dd>Ready In Minutes: '+data.results[i].readyInMinutes+'</dd><dd>Servings: '+data.results[i].servings+'</dd><dd>SourceUrl:<a href="'+data.results[i].sourceUrl+'" target="_blank">Visit Here </a></dd><dd>image:<img src="'+data.baseUri+''+data.results[i].image+'" alt="" style="width: 10%;height: auto;"></dd></dl><hr>';
+          var	dat ='<dl><dd><dt>Recipe:'+data.results[i].title+'</dt><dd>Ready In Minutes: '+data.results[i].readyInMinutes+'</dd><dd>Servings: '+data.results[i].servings+'</dd><dd>SourceUrl:<a href="'+data.results[i].sourceUrl+'" target="_blank">Visit Here </a></dd><dd>image:<img src="'+data.baseUri+''+data.results[i].image+'" alt="" style="width: 30%;height: 10%;"></dd></dl><hr>';
               $('.food').append(dat);
           }
           
-          // alert(data.results);
       }, 
       function(error) {
           console.log(error)
